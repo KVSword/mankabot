961fcc71c3165fd15afd6156da4eaabb8c01c614
pavel2017 aaaaaa <pavel2017 aaaaaa>
2018-05-14 22:45:40 +0300

    deply for joske ky

--- settings.py
+++ settings.py
@@ -79,6 +79,8 @@ class BotSettings(BaseSettings):
         RememberPlugin("напомни",prefixes=prefixes),  # use_db=True, if you can use PeeweePlugin
 
         # Plugins:
+        LockChatPlugin("сохранять", prefixes=prefixes),
+        AzinoPlugin("азино", prefixes=prefixes),
         RussianRoulettePlugin(prefixes=prefixes),
         LockChatPlugin("сохранять", prefixes=prefixes),
         DuelerPlugin(prefixes=prefixes),
