4cee3a9213eb15e9732fa67c077cb9c220ac6598
KVSword <pmanankov2002@gmail.com>
2018-06-16 21:55:31 +0300

    abcde kill me pls

--- plugins/content/video.py
+++ plugins/content/video.py
@@ -2,18 +2,19 @@ from handler.base_plugin_command import CommandPlugin
 
 
 class VideoPlugin(CommandPlugin):
-    __slots__ = ()
+    __slots__ = (1, 2)
 
     def __init__(self, *commands, prefixes=None, strict=False):
         super().__init__(*commands, prefixes=prefixes, strict=strict)
 
         self.description = [
-            "Видео", f"{self.command_example()} [запрос] - поиск видео по запросу"
+            "Видео", f"{self.command_example(1)} [запрос] - поиск видео по запросу"
+			"Музыка", f"{self.command_example(2)} [запрос] - поиск музыки "
         ]
 
     async def process_message(self, msg):
         data = await self.api.video.search(
-            q=self.parse_message(msg, full_text=True)[1] or "Jojo",
+            q=self.parse_message(msg, full_text=False)[1] or "anime.webm Jojo",
             sort=10,
             count=10,
             adult=10
@@ -26,6 +27,24 @@ class VideoPlugin(CommandPlugin):
             'Приятного просмотра!',
             attachment=','.join(
                 f"video{vid['owner_id']}_{vid['id']}"
+                    for vid in data["items"]
+            )
+        )
+    async def process_message(self, msg):
+        data = await self.api.audio.search(
+            q=self.parse_message(msg, full_text=False)[2] or "anime.webm Jojo",
+            sort=10,
+            count=10,
+            adult=10
+        )
+
+        if not data or not data.get("items"):
+            return await msg.answer("Я не могу получить видео или ничего не нашлось!")
+
+        return await msg.answer(
+            'Приятного просмотра!',
+            attachment=','.join(
+                f"audio{vid['owner_id']}_{vid['id']}"
                     for vid in data["items"]
             )
         )
