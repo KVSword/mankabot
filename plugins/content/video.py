b194d12517607b6cdaabb599c297bea5bd7a4743
KVSword <pmanankov2002@gmail.com>
2018-06-16 21:58:56 +0300

    abcde kill me pls

--- plugins/content/video.py
+++ plugins/content/video.py
@@ -2,19 +2,19 @@ from handler.base_plugin_command import CommandPlugin
 
 
 class VideoPlugin(CommandPlugin):
-    __slots__ = (audio, video)
+    __slots__ = ()
 
     def __init__(self, *commands, prefixes=None, strict=False):
         super().__init__(*commands, prefixes=prefixes, strict=strict)
 
         self.description = [
-            "Видео", f"{self.command_example(video)} [запрос] - поиск видео по запросу"
-			"Музыка", f"{self.command_example(audio)} [запрос] - поиск музыки "
+            "Видео", f"{self.command_example(1)} [запрос] - поиск видео по запросу"
+			"Музыка", f"{self.command_example(2)} [запрос] - поиск музыки "
         ]
 
     async def process_message(self, msg):
         data = await self.api.video.search(
-            q=self.parse_message(msg, full_text=False)[video] or "anime.webm Jojo",
+            q=self.parse_message(msg, full_text=False)[1] or "anime.webm Jojo",
             sort=10,
             count=10,
             adult=10
@@ -32,7 +32,7 @@ class VideoPlugin(CommandPlugin):
         )
     async def process_message(self, msg):
         data = await self.api.audio.search(
-            q=self.parse_message(msg, full_text=False)[audio] or "anime.webm Jojo",
+            q=self.parse_message(msg, full_text=False)[2] or "anime.webm Jojo",
             sort=10,
             count=10,
             adult=10
