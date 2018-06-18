d25a12396c7ade79180482654712444daf3f2068
KVSword <pmanankov2002@gmail.com>
2018-06-18 23:40:48 +0300

    testik

--- settings.py
+++ settings.py
@@ -79,8 +79,8 @@ class BotSettings(BaseSettings):
         RememberPlugin("напомни",prefixes=prefixes),   # use_db=True, if you can use PeeweePlugin
 
         # Plugins:
-	    EchoPlugin("хентай", prefixes=prefixes),
-	    LockChatPlugin("сохранять", prefixes=prefixes),
+        HentaiPlugin("хентай", prefixes=prefixes),
+        LockChatPlugin("сохранять", prefixes=prefixes),
         AzinoPlugin("азино", prefixes=prefixes),
         RussianRoulettePlugin(prefixes=prefixes),
         LockChatPlugin("сохранять", prefixes=prefixes),
