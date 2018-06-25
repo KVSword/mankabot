6106ff0137061164957b9debad2913bad2da40b5
KVSword <pmanankov2002@gmail.com>
2018-06-25 21:05:06 +0300

    mankabotik

--- settings.py
+++ settings.py
@@ -106,7 +106,6 @@ class BotSettings(BaseSettings):
         CalculatorPlugin("посчитай", "посч", prefixes=prefixes),
         Audio2TextPlugin(key="1c89f760-adb8-49a0-a06e-6ac5c560cfa2", prefixes=prefixes),
         DispatchPlugin("рассылка", prefixes=prefixes, admins=admins),
-        AudioPlugin("музыка", prefixes=prefixes),
         hp,
 
         # Needs tokens (see plugin's codes, some have defaults):
