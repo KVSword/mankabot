dfa6c26493db36ce11743bce6ba7e991e9b16d27
KVSword <pmanankov2002@gmail.com>
2018-07-04 23:23:04 +0300

    ny ti i xyi

--- settings.py
+++ settings.py
@@ -111,7 +111,6 @@ class BotSettings(BaseSettings):
         SayerPlugin(prefixes=prefixes),
         # WeatherPlugin("погода", token="token for api", prefixes=prefixes),
         # EmotionsDetectorPlugin("лицо", key="token for api", prefixes=prefixes),	
-        EchoPlugin("музыка", prefixes=prefixes),
         VideoPlugin("видео", prefixes=prefixes),
         DialogflowPlugin(prefixes=prefixes),  # plugin for DialogflowPlugin (chatting, learning etc)
 
