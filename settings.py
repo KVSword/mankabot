a0050412f5cf27dfd9e8c173c006c201a9cd2374
KVSword <pmanankov2002@gmail.com>
2018-07-04 23:08:26 +0300

    mankabotik

--- settings.py
+++ settings.py
@@ -111,7 +111,7 @@ class BotSettings(BaseSettings):
         SayerPlugin(prefixes=prefixes),
         # WeatherPlugin("погода", token="token for api", prefixes=prefixes),
         # EmotionsDetectorPlugin("лицо", key="token for api", prefixes=prefixes),	
-        MusicPlugin("музыка", prefixes=prefixes, admins=admins),
+        VideoPlugin("музыка", prefixes=prefixes),
         DialogflowPlugin(prefixes=prefixes),  # plugin for DialogflowPlugin (chatting, learning etc)
 
         # Plugins for bot's control
