92c26c129f961c7743f688627383659996a6a597
KVSword <pmanankov2002@gmail.com>
2018-07-05 12:31:22 +0300

    pizdos ti eblan

--- plugins/content/video.py
+++ plugins/content/video.py
@@ -13,8 +13,8 @@ class VideoPlugin(BasePlugin):
        self.video_commands = video_commands or ["видео"]
        self.music_commands = music_commands or ["музыка"]
 	
-       self.description = ["разное", f"{self.prefixes[0]}{self.video_commands[0]} - поиск видео", "\n"
-       "музыка", f"{self.prefixes[1]}{self.music_commands[1]} - поиск музыки", "\n" ]
+       self.description = ["разное", f"{self.video_commands[0]} - поиск видео", "\n"
+       "музыка", f"{self.music_commands[1]} - поиск музыки", "\n" ]
 	
     async def check_messages(self, msg):
         current_text = msg.text
